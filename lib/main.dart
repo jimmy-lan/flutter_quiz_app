1787239e1b01800e4394563f38beb908c409e4fe
Jimmy <jimmylanbcn@gmail.com>
2021-04-23 13:04:58 -0400

    Convert main widget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  List<String> questions = [
+    "What is your favorite color?",
+    "What is your favorite animal?"
+  ];
+
+  void answerQuestion () {
+    setState(() {
+      if (_questionIndex < questions.length - 1) {
+        _questionIndex++;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<String> questions = [
-      "What is your favorite color?",
-      "What is your favorite animal?"
-    ];
-
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text("Simple Quiz App")),
             body: Column(
               children: [
-                Text("The Question"),
-                ElevatedButton(onPressed: null, child: Text("Answer 1")),
-                ElevatedButton(onPressed: null, child: Text("Answer 2")),
-                ElevatedButton(onPressed: null, child: Text("Answer 3")),
+                Text(questions[_questionIndex]),
+                ElevatedButton(onPressed: answerQuestion, child: Text("Answer 1")),
+                ElevatedButton(onPressed: answerQuestion, child: Text("Answer 2")),
+                ElevatedButton(onPressed: answerQuestion, child: Text("Answer 3")),
               ],
             )
         )
